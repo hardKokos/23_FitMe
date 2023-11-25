897ec1d452b3dfc40bfa886ae4c0f5b4538b0c34
hardKokos <102967897+hardKokos@users.noreply.github.com>
2023-11-25 02:09:36 +0100

    Shortened code and change of idea

--- frontend/fit_me/lib/calendar.dart
+++ frontend/fit_me/lib/calendar.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'events.dart';
-import 'form.dart';
 
 class EventCalendarPage extends StatefulWidget {
-  const EventCalendarPage({super.key});
+  const EventCalendarPage({Key? key}) : super(key: key);
 
   @override
   _EventCalendarPageState createState() => _EventCalendarPageState();
 }
 
 class _EventCalendarPageState extends State<EventCalendarPage> {
-  late final ValueNotifier<List<Event>> _selectedEvents;
-  CalendarFormat _calendarFormat = CalendarFormat.week; // Can be toggled on/off by longpressing a date
+  CalendarFormat _calendarFormat = CalendarFormat.week;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   int _currentIndex = 0;
@@ -20,20 +17,7 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
   @override
   void initState() {
     super.initState();
-
     _selectedDay = _focusedDay;
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
-  }
-
-  @override
-  void dispose() {
-    _selectedEvents.dispose();
-    super.dispose();
-  }
-
-  List<Event> _getEventsForDay(DateTime day) {
-    // Implementation example
-    return kEvents[day] ?? [];
   }
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
@@ -42,13 +26,104 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
         _selectedDay = selectedDay;
         _focusedDay = focusedDay;
       });
-
-      _selectedEvents.value = _getEventsForDay(selectedDay);
     }
+  }
+
+  Widget buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      showUnselectedLabels: true,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.blue,
+      onTap: (int index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.calendar_month,
+            color: Colors.black,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.search,
+            color: Colors.black,
+          ),
+          label: 'Find product',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.create,
+            color: Colors.black,
+          ),
+          label: 'Create diet',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.directions_run,
+            color: Colors.black,
+          ),
+          label: 'Daily activities',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.coffee,
+            color: Colors.black,
+          ),
+          label: 'Water',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.shopping_bag,
+            color: Colors.black,
+          ),
+          label: 'Fit Shops',
+        ),
+      ],
+    );
+  }
+
+  Widget buildRow(String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.blue,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    DateTime threeYearsAgo =
+        DateTime.now().subtract(const Duration(days: 3 * 365));
+    DateTime threeYearsForth =
+        DateTime.now().add(const Duration(days: 3 * 365));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -56,89 +131,23 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
           style: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.bold,
-            color: Colors.black
+            color: Colors.black,
           ),
-          ),
-        backgroundColor: Colors.white,  
+        ),
+        backgroundColor: Colors.white,
         centerTitle: true,
         elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add onPressed event
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context) {
-              return CustomForm();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        showUnselectedLabels: true,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.blue,
-        onTap: (int index){
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.calendar_month,
-              color: Colors.black,
-              ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-              color: Colors.black,
-              ),
-            label: 'Find product',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.create,
-              color: Colors.black,
-              ),
-            label: 'Create diet',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.directions_run, 
-              color: Colors.black,
-              ),
-            label: 'Daily activites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.coffee,
-              color: Colors.black,
-              ),
-            label: 'Water',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.shopping_bag,
-              color: Colors.black,
-              ),
-            label: 'Fit Shops',
-          ),
-        ],
-      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          TableCalendar<Event>(
-            firstDay: kFirstDay,
-            lastDay: kLastDay,
+          TableCalendar(
+            firstDay: threeYearsAgo,
+            lastDay: threeYearsForth,
             focusedDay: _focusedDay,
             selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
             calendarFormat: _calendarFormat,
-            eventLoader: _getEventsForDay,
             startingDayOfWeek: StartingDayOfWeek.monday,
             onDaySelected: _onDaySelected,
             onFormatChanged: (format) {
@@ -152,33 +161,11 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
               _focusedDay = focusedDay;
             },
           ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child: ValueListenableBuilder<List<Event>>(
-              valueListenable: _selectedEvents,
-              builder: (context, value, _) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 4.0,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: ListTile(
-                        onTap: () => print('${value[index]}'), //edit option
-                        title: Text('${value[index]}'),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
+          buildRow('Text 1'),
+          buildRow('Text 2'),
+          buildRow('Text 3'),
+          buildRow('Text 4'),
+          buildRow('Text 5'),
         ],
       ),
     );
