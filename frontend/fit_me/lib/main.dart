1a6eb78797ee40851b456077d920d1f34780dea7
hardKokos <102967897+hardKokos@users.noreply.github.com>
2023-11-30 03:19:23 +0100

    garbage

--- frontend/fit_me/lib/main.dart
+++ frontend/fit_me/lib/main.dart
@@ -5,12 +5,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'dart:async';
 import 'package:fit_me/widget_tree.dart';
 
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const FitMe());
+// }
 
-Future main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const FitMe());
-}
+void main() => runApp(const FitMe());
 
 class FitMe extends StatelessWidget {
   const FitMe({super.key});
@@ -19,7 +20,8 @@ class FitMe extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const WidgetTree(),
+      //home: const WidgetTree(),
+      home: EventCalendarPage(),
     );
   }
 }
