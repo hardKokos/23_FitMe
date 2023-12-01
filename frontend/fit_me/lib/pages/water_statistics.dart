08fe6afb71c29ecf556df288ac75961e082d463b
MarekZuchowski <maarek.zuchowski@gmail.com>
2023-12-01 09:56:39 +0100

    water statistic page

---
+++ frontend/fit_me/lib/pages/water_statistics.dart
@@ -0,0 +1,210 @@
+import 'package:fit_me/pages/create_diet.dart';
+import 'package:flutter/material.dart';
+import 'package:fit_me/calendar.dart';
+import 'package:fit_me/search_for_product.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class WaterStatistics extends StatefulWidget {
+  const WaterStatistics({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _WaterStatisticsState createState() => _WaterStatisticsState();
+}
+
+class _WaterStatisticsState extends State<WaterStatistics> {
+  CalendarFormat _calendarFormat = CalendarFormat.week;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  int _currentIndex = 0;
+  int _waterAmount = 0;
+  // final TextEditingController _controllerWaterToAdd = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+    }
+  }
+
+  Widget build(BuildContext context) {
+    DateTime yearAgo = DateTime.now().subtract(const Duration(days: 365));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Water',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black, // Adjusted text color
+          ),
+        ),
+        backgroundColor: Colors.lime.shade400,
+        centerTitle: true,
+        elevation: 0.0,
+        automaticallyImplyLeading: false,
+      ),
+      backgroundColor: Colors.grey[850],
+
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              firstDay: yearAgo,
+              lastDay: DateTime.now(),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              calendarFormat: _calendarFormat,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              onDaySelected: _onDaySelected,
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+              calendarStyle: CalendarStyle(
+                outsideDaysVisible: false,
+                weekendTextStyle:
+                const TextStyle(color: Colors.lightBlueAccent),
+                holidayTextStyle: const TextStyle(color: Colors.lightBlueAccent),
+                todayDecoration: const BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: const TextStyle(color: Colors.white),
+                selectedDecoration: BoxDecoration(
+                  color: Colors.lightBlueAccent.shade400,
+                  shape: BoxShape.circle,
+                ),
+                defaultTextStyle: const TextStyle(color: Colors.white),
+              ),
+              headerStyle: const HeaderStyle(
+                leftChevronIcon:
+                Icon(Icons.arrow_back_ios, color: Colors.lightBlueAccent),
+                rightChevronIcon:
+                Icon(Icons.arrow_forward_ios, color: Colors.lightBlueAccent),
+                titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+                formatButtonVisible: false,
+              ),
+            ),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(top: 50.0),
+                child: Column(
+                  children: [
+                    Text(
+                      _waterAmount.toString(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 70,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "/ 2000 ml",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _waterAmount += 250;
+                        });
+                       },
+                      child: Text(
+                        "+ 250 ml",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                    ),
+                    // Row(
+                    //   mainAxisSize: MainAxisSize.min,
+                    //   children: <Widget>[
+                    //     SizedBox(
+                    //         width: 100,
+                    //         child: TextField(
+                    //           controller: _controllerWaterToAdd,
+                    //
+                    //         )
+                    //     ),
+                    //
+                    //   ],
+                    // ),
+                  ],
+                ),
+              ),
+            )
+
+          ],
+        )
+      ),
+
+
+      bottomNavigationBar: MyBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SearchForProduct()),
+            );
+          }
+
+          // Firebase - home_page, article_detail_page, article_page, challenges_page
+          // Think about layout of bottomNavigationBar in each page
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const EventCalendarPage()),
+            );
+          }
+
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CreateDietPage()),
+            );
+          }
+        },
+      ),
+
+    );
+  }
+}
