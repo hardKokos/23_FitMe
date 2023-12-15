44555587380aa95d29f99031911209cdae785e51
SzuligowskiM <szuligowskim@gmail.com>
2023-12-15 01:56:11 +0100

    Added TabBar to dailyActivities

--- frontend/fit_me/lib/pages/daily_activities_page.dart
+++ frontend/fit_me/lib/pages/daily_activities_page.dart
@@ -5,58 +5,51 @@ import 'challenges_page.dart';
 class DailyActivitiesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Fit Me',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Fit Me',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        backgroundColor: Colors.lime.shade400,
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Container(
-        color: Colors.grey[850],
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.lime.shade400,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ArticlePage(),
-                    ),
-                  );
-                },
-                child: Text('Go to Articles'),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lime.shade400,
+          backgroundColor: Colors.lime.shade400,
+          centerTitle: true,
+          elevation: 0.0,
+          bottom: const TabBar(
+            
+            indicatorColor: Colors.grey,
+            tabs: [
+              Tab(
+                  child: Text(
+                  'Articles',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChallengesPage(),
-                    ),
-                  );
-                },
-                child: Text('Go to Challenges'),
-                
+                ),
+              ),
+              Tab(
+                  child: Text(
+                  'Challenges',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ],
           ),
+        ),
+        body: TabBarView(
+          children: [
+            ArticlePage(),
+            ChallengesPage(),
+          ],
         ),
       ),
     );
